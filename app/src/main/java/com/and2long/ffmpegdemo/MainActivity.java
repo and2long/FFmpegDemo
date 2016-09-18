31816cd6789d91d9ef4720aabf3775445c8e8748
and2long <and2long@gmail.com>
2016-09-19 00:05:54 +0800

    修改命令

--- app/src/main/java/com/and2long/ffmpegdemo/MainActivity.java
+++ app/src/main/java/com/and2long/ffmpegdemo/MainActivity.java
@@ -1,10 +1,16 @@
 package com.and2long.ffmpegdemo;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +27,7 @@ import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedExceptio
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "MainActivity";
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 1;
     private FFmpeg ffmpeg;
     private Button runButton;
     private ProgressDialog progressDialog;
@@ -44,8 +51,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         videoPath = "/sdcard/video.mp4";
         audioPath = "/sdcard/audio.aac";
         outputPath = "/sdcard/output.mp4";
-        String cmd = "-y -i " + videoPath + " -i " + audioPath + " -filter_complex \"[0:a] pan=stereo|c0=1*c0|c1=1*c1 [a1], [1:a] pan=stereo|c0=1*c0|c1=1*c1 [a2],[a1][a2]amix=duration=first,pan=stereo|c0<c0+c1|c1<c2+c3,pan=mono|c0=c0+c1[a]\" -map \"[a]\" -map 0:v -c:v copy -c:a aac -strict -2 -ac 2 " + outputPath;
-        //String cmd = "-y -i " + videoPath + " -c copy " + outputPath;
+        //String cmd = "-y -i " + videoPath + " -i " + audioPath + " -filter_complex \"[0:a] pan=stereo|c0=1*c0|c1=1*c1 [a1], [1:a] pan=stereo|c0=1*c0|c1=1*c1 [a2],[a1][a2]amix=duration=first,pan=stereo|c0<c0+c1|c1<c2+c3,pan=mono|c0=c0+c1[a]\" -map \"[a]\" -map 0:v -c:v copy -c:a aac -strict -2 -ac 2 " + outputPath;
+        String cmd = "-y -i " + videoPath + " -i " + audioPath + " -filter_complex amix=inputs=2:duration=first:dropout_transition=2 -c:v copy -c:a aac " + outputPath;
         System.out.println(cmd);
         command = cmd.split(" ");
 
@@ -56,6 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         runButton.setOnClickListener(this);
         outputLayout = (LinearLayout) findViewById(R.id.command_output);
         progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
     }
 
     /**
@@ -101,10 +109,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
      */
     @Override
     public void onClick(View v) {
+        checkPermissionsAndRunCommand();
+    }
+
+    /**
+     * 检查权限运行命令
+     */
+    private void checkPermissionsAndRunCommand() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            //申请WRITE_EXTERNAL_STORAGE权限
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        } else {
+            runCommand();
+        }
+
+    }
+
+    /**
+     * 运行命令
+     */
+    private void runCommand() {
         if (command.length != 0) {
             execFFmpegBinary(command);
         } else {
             Toast.makeText(this, getString(R.string.empty_command_toast), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * 权限请求结果
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        doNext(requestCode, grantResults);
+    }
+
+    /**
+     * 下一步操作
+     * @param requestCode
+     * @param grantResults
+     */
+    private void doNext(int requestCode, int[] grantResults) {
+        if (requestCode == WRITE_EXTERNAL_STORAGE_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission Granted
+                runCommand();
+            } else {
+                // Permission Denied
+                Snackbar.make(runButton, "需要内存卡读写权限", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("OK", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                            }
+                        }).show();
+            }
         }
     }
 
